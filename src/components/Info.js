7efd28a576139d7b85ef477967e6aead63af97a2
My Name <azs030369@gmail.com>
2023-01-04 19:05:31 +0900

    add movie_info

---
+++ src/components/Info.js
@@ -0,0 +1,13 @@
+function Info({ title, img, download_count, language, description_full }) {
+  return (
+    <div className="Info">
+      <h1>{title}</h1>
+      <h1>img</h1>
+      <h1>downloadcount</h1>
+      <h1>language</h1>
+      <h1>description full</h1>
+    </div>
+  );
+}
+
+export default Info;
--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,18 +1,33 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import Info from "../components/Info";
 
 function Detail() {
+  const [moviesInfo, setMoviesInfo] = useState([]);
   const { id } = useParams();
   const getMovie = async () => {
     const json = await (
       await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
     ).json();
     console.log(json);
-    console.log(json);
+    setMoviesInfo(json.data.movie);
   };
   useEffect(() => {
     getMovie();
   }, []);
-  return <h1>Detail</h1>;
+  return (
+    <div className="Detail">
+      {moviesInfo.map((movieInfo) => (
+        <Info
+          key={movieInfo.id}
+          title={movieInfo.title_long}
+          img={movieInfo.large_cover_image}
+          download_count={movieInfo.download_count}
+          language={movieInfo.language}
+          description_full={movieInfo.description_full}
+        />
+      ))}
+    </div>
+  );
 }
 export default Detail;
